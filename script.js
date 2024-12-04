599e40fc193058bfac11fcedb7c248cc134c2f04
Eve <62806964+TheCoolerMS3@users.noreply.github.com>
2024-12-04 14:03:58 +0000

    Update script.js

--- script.js
+++ script.js
@@ -1,27 +1,6 @@
 async function fetchNowPlaying() {
-  let nowplayingtext = "Getting data!";
-  const lastfm_username = 'eveisinsane';
-  const lastfm_apiKey = '9d9d0986fbd8191b0f60c42032ac6ac8';
-  const lastfm_apiurl = `https://ws.audioscrobbler.com/2.0/?method=user.getrecenttracks&user=${lastfm_username}&api_key=${lastfm_apiKey}&format=json&limit=1`;
-
-  if (
-    /Mobi|Android|iPhone|iPad|iPod/i.test(navigator.userAgent) && localStorage.getItem("preferDesktop") !== "true"){
-    window.location.href = "https://bottom.monster/mobile";
-}
-
-  try {
-    const response = await fetch(lastfm_apiurl);
-    const data = await response.json();
-
-    const track = data.recenttracks.track[0];
-
-    if (track && track['@attr'] && track['@attr'].nowplaying) {
-      const artist = track.artist['#text'];
-      const songTitle = track.name;
-
-      nowplayingtext = `I am currently listening to ${songTitle} by ${artist}`;
-    } else {
-      let randnumber = Math.random() * 13;
+  let nowplayingtext = "";
+  let randnumber = Math.random() * 13;
       randnumber = Math.round(randnumber);
       if (randnumber === 1){
         nowplayingtext = "DIY or die!!"
@@ -62,7 +41,27 @@ async function fetchNowPlaying() {
       else if (randnumber === 13){
         nowplayingtext = "Easter eggs are in check!"
       }
-    }
+  const lastfm_username = 'eveisinsane';
+  const lastfm_apiKey = '9d9d0986fbd8191b0f60c42032ac6ac8';
+  const lastfm_apiurl = `https://ws.audioscrobbler.com/2.0/?method=user.getrecenttracks&user=${lastfm_username}&api_key=${lastfm_apiKey}&format=json&limit=1`;
+
+  if (
+    /Mobi|Android|iPhone|iPad|iPod/i.test(navigator.userAgent) && localStorage.getItem("preferDesktop") !== "true"){
+    window.location.href = "https://bottom.monster/mobile";
+}
+
+  try {
+    const response = await fetch(lastfm_apiurl);
+    const data = await response.json();
+
+    const track = data.recenttracks.track[0];
+
+    if (track && track['@attr'] && track['@attr'].nowplaying) {
+      const artist = track.artist['#text'];
+      const songTitle = track.name;
+
+      nowplayingtext = `I am currently listening to ${songTitle} by ${artist}`;
+    } 
   } catch (error) {
     console.error('Error fetching now playing data:', error);
     nowplayingtext = 'API Error! Check console...';
