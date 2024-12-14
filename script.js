934fd57c82d9c8238cc378d3cc949477390bc437
Eve <62806964+TheCoolerMS3@users.noreply.github.com>
2024-12-14 11:14:01 +0000

    a

--- script.js
+++ script.js
@@ -29,7 +29,7 @@ async function getSplashVariable() {
 }
 
 getSplashVariable();
-document.getElementById("splash").innerHTML = splash;
+document.getElementById("thesplash").innerHTML = splash;
 
 function discord() {
   alert("My discord name is @lenenjoyerr , I also have an account for Pxls called @greenwizardfan");
